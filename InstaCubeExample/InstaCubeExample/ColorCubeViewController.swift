6051c4e51ea6c05f8a3db849e2be0f8168a6d5a3
jake.vanorder@gmail.com <jake.vanorder@gmail.com>
2014-06-15 16:31:53 -0500

    Added tableView for selection of colorCube.

--- InstaCubeExample/InstaCubeExample/ColorCubeViewController.swift
+++ InstaCubeExample/InstaCubeExample/ColorCubeViewController.swift
@@ -11,16 +11,14 @@ import UIKit
 import CoreImage
 import InstaCube
 
-class ColorCubeViewController : UIViewController {
+class ColorCubeViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var mainImageView : UIImageView
-    var mainColorCubeFilter : CIFilter
+    @IBOutlet var colorCubeSelectionColorCube : UITableView
     var currentFilter : ColorCubeFilterList
     
     init(coder aDecoder: NSCoder!) {
         currentFilter = ColorCubeFilterList.vibrance
-        let keyImage = UIImage(named: currentFilter.toRaw())
-        mainColorCubeFilter = InstaCubeGenerator.instaCubeWithKeyImage(keyImage)
         super.init(coder: aDecoder)
     }
     
@@ -28,13 +26,81 @@ class ColorCubeViewController : UIViewController {
         super.viewDidLoad()
     }
     
-    @IBAction func actionButtonWasTapped(sender : UIButton) {
-        let incomingImage = CIImage(CGImage: self.mainImageView.image.CGImage)
-        mainColorCubeFilter.setValue(incomingImage, forKey: kCIInputImageKey)
-        let outgoingImage = mainColorCubeFilter.valueForKey(kCIOutputImageKey) as CIImage
+    func generateColorCubeWithFilterList(colorCubeFilter  : ColorCubeFilterList) -> CIFilter {
+        currentFilter = colorCubeFilter
+        let keyImage = UIImage(named: colorCubeFilter.toRaw())
+        return InstaCubeGenerator.instaCubeWithKeyImage(keyImage)
+    }
+    
+    func filterImageWithColorCube(colorCubeFilter : CIFilter) {
+
+        let rawImage = UIImage(named: "example").CGImage
+        if let checkedRawImage = rawImage {
+            if let ciImage = CIImage(CGImage: checkedRawImage) as CIImage? {
+                colorCubeFilter.setValue(ciImage, forKey: kCIInputImageKey)
+            }
+            else {
+                return
+            }
+        }
+        else {
+            return
+        }
+        
+        let outgoingImage = colorCubeFilter.valueForKey(kCIOutputImageKey) as CIImage
         mainImageView.image = UIImage(CIImage: outgoingImage)
     }
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return ColorCubeFilterList.allValues.count
+    }
+        
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        
+        let tableViewCell : UITableViewCell! = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        let filter = ColorCubeFilterList.allValues[indexPath.row]
+        tableViewCell.textLabel.text = filter.displayName
+        
+        return tableViewCell
+    }
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        let filter = ColorCubeFilterList.allValues[indexPath.row]
+        let colorCubeFilter = generateColorCubeWithFilterList(filter)
+        filterImageWithColorCube(colorCubeFilter)
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
 }
+
+/*
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return <#number#>;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView
+ numberOfRowsInSection:(NSInteger)section
+{
+    return <#number#>
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#reuseIdentifier#> forIndexPath:<#indexPath#>];
+
+    [self configureCell:cell forRowAtIndexPath:indexPath];
+
+    return cell;
+}
+
+- (void)configureCell:(UITableViewCell *)cell
+    forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    <#statements#>
+}
+*/
 
 enum ColorCubeFilterList : String {
     case solarize = "colorCube_solarize"
